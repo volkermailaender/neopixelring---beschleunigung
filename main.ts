716ff069b1aac712e114769daaa71d81613177b8
volkermailaender <volker.mailaender@gmx.de>
2022-10-08 12:36:30 +0200

    Einzelnen LEDs werden zufällig Farben zugeteilt

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-strip.setBrightness(100)
+strip.setBrightness(20)
 strip.show()
 basic.forever(function () {
     for (let Index = 0; Index <= 12; Index++) {
-        strip.showColor(neopixel.rgb(input.acceleration(Dimension.X) / 70, input.acceleration(Dimension.Y) / 70, input.acceleration(Dimension.Z) / 70))
+        strip.setPixelColor(Index, neopixel.rgb(randint(-20, 255), randint(-20, 255), randint(-20, 255)))
         strip.show()
         basic.pause(10)
     }
